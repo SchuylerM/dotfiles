1efac3a3add3ee74ef3fcbc4c91558132ffe7c68
Schuyler Manchester <schuyler.manchester@gmail.com>
2019-12-25 01:23:38 +0000

    Adding MariaDB install.

--- install.sh
+++ install.sh
@@ -15,3 +15,12 @@ fi
 
 # Django
 sudo python3 -m pip install Django
+
+# MariaDB
+# From https://downloads.mariadb.org/mariadb/repositories/#distro=Debian&distro_release=stretch--stretch&mirror=mva&version=10.4
+sudo apt-get install software-properties-common dirmngr -y
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xF1656F24C74CD1D8
+sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mirror.mva-n.net/mariadb/repo/10.4/debian stretch main'
+
+sudo apt-get update
+sudo apt-get install mariadb-server -y
