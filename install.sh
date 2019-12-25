4c9f3e2180378766bc8b0ac232b8667d130e60aa
Schuyler Manchester <schuyler.manchester@gmail.com>
2019-12-25 01:14:04 +0000

    Adding pip and django to install.

--- install.sh
+++ install.sh
@@ -6,3 +6,12 @@ sudo apt install tree -y
 # pbcopy / pbpaste for linux (bashrc/aliases will make it so you can use pbcopy/pbpaste)
 sudo apt install xclip xsel -y
 
+# Pip
+if ! [ -x "$(command -v pip)" ]; then
+  echo 'Pip not installed.  Installing...'
+  curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+  sudo python3 /tmp/get-pip.py
+fi
+
+# Django
+sudo python3 -m pip install Django
